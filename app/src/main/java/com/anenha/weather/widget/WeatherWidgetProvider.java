0de9d4515e3bf8dc54a509d48a894996cb503877
Ana Jessica Saragossa <aj.nenha@gmail.com>
2017-10-24 20:30:09 -0200

    widget

--- app/src/main/java/com/anenha/weather/widget/WeatherWidgetProvider.java
+++ app/src/main/java/com/anenha/weather/widget/WeatherWidgetProvider.java
@@ -1,7 +1,5 @@
 package com.anenha.weather.widget;
 
-import java.util.Random;
-
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
@@ -12,28 +10,43 @@ import android.util.Log;
 import android.widget.RemoteViews;
 
 import com.anenha.weather.R;
+import com.anenha.weather.app.entity.TodayEntity;
+import com.anenha.weather.app.model.Channel;
+import com.anenha.weather.app.provider.yahooWeather.WeatherServiceCallback;
+import com.anenha.weather.app.provider.yahooWeather.YahooWeatherService;
+import com.anenha.weather.app.utils.Prefs;
 
-public class WeatherWidgetProvider extends AppWidgetProvider {
+public class WeatherWidgetProvider extends AppWidgetProvider implements WeatherServiceCallback {
+    private Context context;
+    private RemoteViews remoteViews;
+    private AppWidgetManager appWidgetManager;
+    private int[] allWidgetIds;
 
-       private static final String ACTION_CLICK = "ACTION_CLICK";
+    @Override
+    public void onEnabled(Context context) {
+        super.onEnabled(context);
+        Log.d("WIDGET", "ADDED!");
+    }
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager,
                          int[] appWidgetIds) {
+        Log.d("WIDGET", "UPDATED!");
+
+        this.context = context;
+        this.appWidgetManager = appWidgetManager;
+
+        YahooWeatherService weatherService = new YahooWeatherService(this);
+        weatherService.refreshWeather(Prefs.getInitialCity(context));
+
 
         // Get all ids
-        ComponentName thisWidget = new ComponentName(context,
-                WeatherWidgetProvider.class);
-        int[] allWidgetIds = appWidgetManager.getAppWidgetIds(thisWidget);
+        ComponentName thisWidget = new ComponentName(context, WeatherWidgetProvider.class);
+        allWidgetIds = appWidgetManager.getAppWidgetIds(thisWidget);
         for (int widgetId : allWidgetIds) {
-            // create some random data
-            int number = (new Random().nextInt(100));
-
-            RemoteViews remoteViews = new RemoteViews(context.getPackageName(),
-                    R.layout.activity_widget_layout);
-            Log.w("WidgetExample", String.valueOf(number));
-            // Set the text
-            remoteViews.setTextViewText(R.id.update, String.valueOf(number));
+            remoteViews = new RemoteViews(context.getPackageName(), R.layout.layout_widget);
+            remoteViews.setTextViewText(R.id.widget_condition, context.getString(R.string.app_loading));
+            remoteViews.setTextViewText(R.id.widget_locale, "");
 
             // Register an onClickListener
             Intent intent = new Intent(context, WeatherWidgetProvider.class);
@@ -43,8 +56,35 @@ public class WeatherWidgetProvider extends AppWidgetProvider {
 
             PendingIntent pendingIntent = PendingIntent.getBroadcast(context,
                     0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-            remoteViews.setOnClickPendingIntent(R.id.update, pendingIntent);
+            remoteViews.setOnClickPendingIntent(R.id.widget_container, pendingIntent);
             appWidgetManager.updateAppWidget(widgetId, remoteViews);
         }
     }
-} 
+
+    @Override
+    public void serviceSuccess(Channel channel) {
+        new TodayEntity(context, channel, new TodayEntity.TodayCallback() {
+            @Override
+            public void onCreate(TodayEntity te) {
+                remoteViews.setImageViewResource(R.id.widget_image, te.getImageResource());
+                remoteViews.setTextViewText(R.id.widget_locale, te.getLocal(false));
+                remoteViews.setTextViewText(R.id.widget_condition, te.getCondition());
+
+                for (int widgetId : allWidgetIds) {
+                    appWidgetManager.updateAppWidget(widgetId, remoteViews);
+                }
+            }
+        });
+    }
+
+    @Override
+    public void serviceFailure(Exception exception) {
+        remoteViews.setImageViewResource(R.id.widget_image, R.drawable.icon_na);
+        remoteViews.setTextViewText(R.id.widget_locale, "");
+        remoteViews.setTextViewText(R.id.widget_condition, "Falha ao atualizar!");
+
+        for (int widgetId : allWidgetIds) {
+            appWidgetManager.updateAppWidget(widgetId, remoteViews);
+        }
+    }
+}
